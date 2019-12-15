361f55d05d05c2035e0d2bb7e9958f6b27abe39c
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 05:18:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)# code an argument here
+  planeteer.map do |i|
+    i << "!"# Your code here
 end
 
 def long_planeteer_calls# code an argument here
