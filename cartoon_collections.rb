04b0cc91d22dda806cfddeb1071c4cd6fbf0673b
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 05:33:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(calls_long)# code an argument here
   calls_long.any?{|i| i.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
