1af77ea2656239601de9b7f01ed7d72a3edd265f
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 04:59:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
 
-  dwarves.each_with_index do {|i, index|
+  while dwarves.each_with_index do |i|
     puts dwarves[i]
     i += 1# Your code here
   }
